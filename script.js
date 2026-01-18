e81c0475bfdec71010282a6f78ac614ac6964cd6
shwetha srinivasan <shwetha12206@gmail.com>
2026-01-18 22:01:04 +0530

    Initial To-Do List App

---
+++ script.js
@@ -0,0 +1,23 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value;
+
+    if (taskText === "") {
+        alert("Enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = taskText;
+
+    let delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
+    delBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(delBtn);
+    document.getElementById("taskList").appendChild(li);
+
+    taskInput.value = "";
+}
